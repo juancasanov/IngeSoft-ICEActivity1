e9fa77e256d71f92a9b0b5aa52ebf44928eb7c0f
Daniel Jose Plazas <danieljpc19@gmail.com>
2025-02-19 16:25:46 -0500

    Upload latency in Client.java

--- printer/client/src/main/java/Client.java
+++ printer/client/src/main/java/Client.java
@@ -1,5 +1,8 @@
 public class Client {
     public static void main(String[] args) {
+        
+        long startTimeInns = System.nanoTime();
+
         try(com.zeroc.Ice.Communicator communicator = com.zeroc.Ice.Util.initialize(args)) {
             com.zeroc.Ice.ObjectPrx base = communicator.stringToProxy("SimplePrinter:default -p 10000");
             Demo.PrinterPrx printer = Demo.PrinterPrx.checkedCast(base);
@@ -8,5 +11,12 @@ public class Client {
             } 
             printer.printString("Hello world");
         }
+        
+
+        long endTimeInns = System.nanoTime();
+
+        System.out.println("Time taken in ns: " + (endTimeInns - startTimeInns));
+        System.out.println("Time taken in ms: " + (endTimeInns - startTimeInns) / 1000000);
+        System.out.println("Time taken in s: " + (endTimeInns - startTimeInns) / 1000000000);
     } 
 }
