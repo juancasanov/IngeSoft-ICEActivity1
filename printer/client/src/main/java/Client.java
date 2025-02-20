1c730352b8e753aae3b23af26b1542b8069a428d
mcordoba12 <angela6309gonzalez@gmail.com>
2025-02-20 13:36:34 -0500

    creation of Throughput

--- printer/client/src/main/java/Client.java
+++ printer/client/src/main/java/Client.java
@@ -1,22 +1,30 @@
 public class Client {
     public static void main(String[] args) {
-        
+
         long startTimeInns = System.nanoTime();
 
-        try(com.zeroc.Ice.Communicator communicator = com.zeroc.Ice.Util.initialize(args)) {
+        try (com.zeroc.Ice.Communicator communicator = com.zeroc.Ice.Util.initialize(args)) {
             com.zeroc.Ice.ObjectPrx base = communicator.stringToProxy("SimplePrinter:default -p 10000");
             Demo.PrinterPrx printer = Demo.PrinterPrx.checkedCast(base);
             if (printer == null) {
                 throw new Error("Invaid proxy");
-            } 
-            printer.printString("Hello world");
+            }
+            String message = "Hello world";
+            printer.printString(message);
+
+            long messageSize = message.getBytes().length;
+
+            long endTimeInns = System.nanoTime();
+
+            long latency = endTimeInns - startTimeInns;
+
+            double throughput = messageSize / latency;
+
+            System.out.println("Time taken in ns: " + (latency));
+            System.out.println("Time taken in ms: " + (latency) / 1000000);
+            System.out.println("Time taken in s: " + (latency) / 1000000000);
+            System.out.println("Tamaño del mensaje en bytes: " + messageSize);
+            System.out.println("Throughput: " + throughput + " bytes/segundo");
         }
-        
-
-        long endTimeInns = System.nanoTime();
-
-        System.out.println("Time taken in ns: " + (endTimeInns - startTimeInns));
-        System.out.println("Time taken in ms: " + (endTimeInns - startTimeInns) / 1000000);
-        System.out.println("Time taken in s: " + (endTimeInns - startTimeInns) / 1000000000);
-    } 
+    }
 }
